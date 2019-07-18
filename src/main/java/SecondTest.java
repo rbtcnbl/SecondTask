6ac1c4d269a2989993c8cab979aa3f9f6ce8e2aa
Alina <alina.alina.29@mail.ru>
2019-07-18 17:36:38 +0300

    fillinputSection

--- src/main/java/SecondTest.java
+++ src/main/java/SecondTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -5,16 +6,22 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
 public class SecondTest {
     WebDriver driver;
 
     @Test
     public void test() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("start-maximized");
-        driver = new ChromeDriver(options);
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("start-maximized");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
         driver.get("http://www.sberbank.ru/ru/person");
+
 
         Thread.sleep(5000);
         JavascriptExecutor js = (JavascriptExecutor) driver;
@@ -22,31 +29,100 @@ public class SecondTest {
 
 
         //страхование
-        WebElement spanBtn = driver.findElement(By.xpath("//span[contains(text(), 'Страхование')]"));
-        spanBtn.click();
+        clickBtn("//span[contains(text(), 'Страхование')]");
+
 
         //Путешествия и покупки
-        WebElement strBtn = driver.findElement(By.xpath("//li[contains(@class, 'lg-menu__sub-item')]/a[contains(text(), 'Путешествия и покупки')]"));
-        strBtn.click();
+        clickBtn("//li[contains(@class, 'lg-menu__sub-item')]/a[contains(text(), 'Путешествия и покупки')]");
+
 
         //оформить онлайн
-        WebElement onlineBtn = driver.findElement(By.xpath("//div[contains(@class, 'sbrf-rich-outer')]/following::a[1]"));
-        onlineBtn.click();
+
+        Set<String> h1 = driver.getWindowHandles();
+        driver.findElement(By.xpath("//div[contains(@class, 'sbrf-rich-outer')]/following::a[1]")).click();
+        Set<String> h2 = driver.getWindowHandles();
+        h2.removeAll(h1);
+        String h3 = h2.iterator().next();
+        driver.switchTo().window(h3);
 
 
-        //ВОТ ТУТ Я СОВСЕМ ЗАПУТАЛАСЬ
-        //driver.switchTo().window(driver.getWindowHandle());
+        //выбрать сумму страховой защиты ??
+        ////div[contains(@class, 'b-form-prog-box b-form-active-box')]
+        waitSectionToDownload();
+
+        //оформить
+        clickBtn("//span[contains(text(), 'Оформить')]");
 
 
-        //WebDriverWait wait = new WebDriverWait(driver, 5, 200);
-        //wait.until(ExpectedConditions.elementToBeClickable(By.className("lg-menu__sub")));
-        //wait.until(ExpectedConditions.);
+        fillInputBySection("1", "insured0_surname", "Gorbulina");
 
-        //минимальная
-        WebElement minCheck = driver.findElement(By.xpath("//div[contains(@class, 'b-form-prog-box b-form-active-box')]"));
-        minCheck.click();
+//        WebElement srnLine = driver.findElement(By.xpath("//input[contains(@name, 'insured0_surname')]"));
+//        String text1 = "Gorbulina";
+//        srnLine.sendKeys(text1);
+
+
+
+
+
+//        InputPaspot("Серия", "123456");
+//        InputPaspot("Номер", "123456");
+//        fillInputBySection("issueDate", "12052014");
+//        InputPaspot("Кем выдан", "фывафыва");
+
+
+        clickBtn("//span[contains(text(), 'Продолжить')]");
+
+//        Assert.assertTrue(driver.findElement(By.xpath("//*[text() = 'Заполнены не все обязательные поля']")).isEnabled());
+
+        checkError("phone", "некорректные данные");
 
         driver.close();
         driver.quit();
     }
+
+    public void clickBtn(String xpath) {
+        driver.findElement(By.xpath(xpath)).click();
+    }
+
+    public String fillInputBySection(String numbOfSection, String name, String textToFill) {
+        String path = String.format("//section[contains(@class, 'b-form-section')][%s]//input[contains(@name, '%s')]");
+        driver.findElement(By.xpath(path)).sendKeys(textToFill);
+        return driver.findElement(By.xpath(path)).getAttribute("value placeholder");
+//        String temp = "//*[contains(@name, '$s')]";
+//        String fulltext = String.format(temp, name);
+//        driver.findElement(By.xpath(fulltext)).sendKeys(textToFill);
+    }
+
+//    public String InputPaspot(String section, String name, String textToFill) {
+//        String path;
+//        if (name.equals("Имя") || name.equals("Серия") ||name.equals("Номер")) {
+//            return fillInputBySection(section, name, textToFill);
+//
+//
+//        driver.findElement(By.xpath(path)).sendKeys(textToFill);
+//        return driver.findElement(By.xpath(path)).getAttribute("value placeholder");
+
+
+////        String temp = "//*[contains(@placeholder, '$s')]";
+////        String fulltext = String.format(temp, name);
+////        driver.findElement(By.xpath(fulltext)).sendKeys(textToFill);
+//    }
+
+    public void checkError(String name, String expectedText) {
+        String template = "//input[contains(@name, 'phone')]/following::span[contains(@class, 'b-text-field-error')]";
+        String fullxpath = String.format(template, name);
+        String actualText = driver.findElement(By.xpath(fullxpath)).getText();
+
+        Assert.assertEquals(expectedText, actualText);
+    }
+
+    public void waitSectionToDownload() {
+        WebDriverWait wait = new WebDriverWait(driver, 5000, 200);
+        wait.until(ExpectedConditions.attributeToBeNotEmpty(driver.findElement(By.xpath("//section[contains(@class, 'b-active-tab')]")), "class"));
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//section[contains(@class, 'b-form-section')][2]"))));
+        WebElement myElement = driver.findElement(By.xpath("//section[contains(@class, 'b-active-tab')]"));
+        JavascriptExecutor jse = (JavascriptExecutor) driver;
+        jse.executeScript("arguments[0].scrollIntoView()", myElement);
+    }
+
 }
